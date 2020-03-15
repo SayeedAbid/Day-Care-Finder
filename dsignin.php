e1a5e5e59152bd7652f6d2ef5bcbc43c662592a6
Md. Sayeed Abid <sayeedabids@gmail.com>
2020-03-16 00:19:03 +0600

    Day Care sign in

--- dsignin.php
+++ dsignin.php
@@ -1,25 +1,29 @@
 <?php
 
-	error_reporting (E_ALL ^ E_NOTICE); 
-
-$username = $_POST['username'];	
-	$password = $_POST['password'];
-
 	$conn = new mysqli('localhost', 'root', '', 'daycaredb');
-	if ($conn->connect_error) {
-		die('Connection Failed : '.$conn->connect_error);
-	}
-	else{
-		$sql="SELECT * FROM daycare WHERE username='$username' AND password ='$password'";
-$result=$conn->query($sql);
 
-if(!$row=$result->fetch_array()){
-	header("Location:dlogin_error.html");
-	//echo '<script type="text/javascript"> alert("Error : Username or password incorrect.") </script>';
-}
-else{
-	$_SESSION['username']=$_POST['username'];
-	header("Location:daycareprofile.php");
-}
-}
+	if (isset($_POST['log'])) {
+		$userId = mysqli_real_escape_string($conn, $_POST['userId']);
+		$password = mysqli_real_escape_string($conn, $_POST['password']);
+
+		$password = md5($password);
+
+		if ($userId!="" && $password!="") {
+
+			$sql = "SELECT * FROM daycare WHERE user_id = '$userId' AND user_password = '$password'";
+			$result = mysqli_query($conn, $sql);
+			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+			$count = mysqli_num_rows($result);
+			if ($count==1) {
+				header("Location:Daycareprofile.html");
+				//echo "Login Successful";
+			}
+			else {
+				header("Location:index.html");
+				//echo "Login Failed";
+			}
+		}
+	}
+
 ?>
