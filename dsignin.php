4af76caad2814f93dc0441883720c981b980aead
Md. Sayeed Abid <sayeedabids@gmail.com>
2020-03-21 12:53:50 +0600

    Sign in files

--- dsignin.php
+++ dsignin.php
@@ -3,27 +3,43 @@
 	$conn = new mysqli('localhost', 'root', '', 'daycaredb');
 
 	if (isset($_POST['log'])) {
-		$userId = mysqli_real_escape_string($conn, $_POST['userId']);
-		$password = mysqli_real_escape_string($conn, $_POST['password']);
 
-		$password = md5($password);
+	    $userID = $_POST['userID'];
+	    $password = $_POST['password'];
 
-		if ($userId!="" && $password!="") {
+	    $password = md5($password);
 
-			$sql = "SELECT * FROM daycare WHERE user_id = '$userId' AND user_password = '$password'";
-			$result = mysqli_query($conn, $sql);
-			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $query = "SELECT * FROM daycare WHERE user_id=? AND user_password=? LIMIT 1";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('is', $userID, $password);
 
-			$count = mysqli_num_rows($result);
-			if ($count==1) {
-				header("Location:Daycareprofile.html");
-				//echo "Login Successful";
-			}
-			else {
-				header("Location:index.html");
-				//echo "Login Failed";
-			}
-		}
+	        if ($stmt->execute()) {
+
+	        	$result = $stmt->get_result();
+		        $user = $result->fetch_assoc();
+		 	    //$stmt->close();
+
+	        	if ($user['verified'] == 1) {
+	        		
+		            $_SESSION['userID'] = $user['userID'];
+	                $_SESSION['message'] = 'You are logged in!';
+	                $_SESSION['type'] = 'alert-success';
+	                header('location: userprofile.php');
+	                exit(0);	
+	        	}
+	        	else
+	        		echo "Email is not verified";
+	            
+            }
+
+	        else { // if password does not match
+	                $errors['login_fail'] = "Wrong userID / password";
+	        }
 	}
+
+    else {
+            $_SESSION['message'] = "Database error. Login failed!";
+            $_SESSION['type'] = "alert-danger";
+        }
 
 ?>
--- signin.php
+++ signin.php
@@ -3,27 +3,44 @@
 	$conn = new mysqli('localhost', 'root', '', 'daycaredb');
 
 	if (isset($_POST['log'])) {
-		$username = mysqli_real_escape_string($conn, $_POST['username']);
-		$password = mysqli_real_escape_string($conn, $_POST['password']);
 
-		$password = md5($password);
+	    $userID = $_POST['userID'];
+	    $password = $_POST['password'];
 
-		if ($username!="" && $password!="") {
+	    $password = md5($password);
 
-			$sql = "SELECT * FROM parent WHERE user_name = '$username' AND u_password = '$password'";
-			$result = mysqli_query($conn, $sql);
-			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $query = "SELECT * FROM parent WHERE u_ID=? AND u_password=? LIMIT 1";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('is', $userID, $password);
 
-			$count = mysqli_num_rows($result);
-			if ($count==1) {
-				header("Location:userprofile.php");
-				//echo "Login Successful";
-			}
-			else {
-				header("Location:index.html");
-				//echo "Login Failed";
-			}
-		}
+	        if ($stmt->execute()) {
+
+	        	$result = $stmt->get_result();
+		        $user = $result->fetch_assoc();
+		 	    //$stmt->close();
+
+	        	if ($user['verified'] == 1) {
+	        		
+		            $_SESSION['userID'] = $user['userID'];
+	                $_SESSION['message'] = 'You are logged in!';
+	                $_SESSION['type'] = 'alert-success';
+	                header('location: userprofile.php');
+	                exit(0);	
+	        	}
+	        	else
+	        		echo "Email is not verified";
+	            
+            }
+
+	        else { // if password does not match
+	                $errors['login_fail'] = "Wrong userID / password";
+	        }
 	}
+
+    else {
+            $_SESSION['message'] = "Database error. Login failed!";
+            $_SESSION['type'] = "alert-danger";
+        }
+
 
 ?>
