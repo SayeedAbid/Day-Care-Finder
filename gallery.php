90ba1da2dc210e3af96c47448b65d93dd78b7f3d
RusafaDosta <60016812+RusafaDosta@users.noreply.github.com>
2020-03-22 15:18:09 +0600

    Image upload file in daycare profile

---
+++ gallery.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>Profile</title>
+	<link href="css/jquery.bxslider.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet"/>
+	<link rel="shortcut icon" type="image/png" href="img/icons/favicon.png"/>
+
+</head>
+<link rel="stylesheet" href="profiles.css">
+<body>
+    <body style="background-image: url(uploads/daycareprofile.jpg)">
+	<header>
+		<div class="container">
+			<div id="branding">
+				<a href="Daycareprofile.html"><h1>Kid Care!</h1></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="postOffer.html"><button class="button">Post Offer</button></a></li>
+					<li><a href="deditprofile.html"><button class="button">Edit Profile</button></a></li>
+					<li><a href="Message.html"><button class="button">Message</button></a></li>
+                    <li><a href="index.html"><button class="button">Sing Out</button></a></li>
+            
+                    
+				</ul>
+			</nav>
+		</div>
+        </header>
+         <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+            <button type="submit" name="submit">UPLOAD</button>
+        
+        
+        </form>  
+	
+    </html>
+    
+
