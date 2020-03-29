bd68b7d9428ec8149d1a4b3105b5fa30b2201e4b
RusafaDosta <60016812+RusafaDosta@users.noreply.github.com>
2020-03-29 13:52:37 +0600

    image upload added

--- deditprofile.php
+++ deditprofile.php
@@ -1,7 +1,5 @@
 <?php
 
-include "uploads.php";
-
 	error_reporting (E_ALL ^ E_NOTICE); 
 
 	$conn = new mysqli('localhost', 'root', '', 'daycaredb'); 
@@ -33,6 +31,41 @@ include "uploads.php";
 	                $errors['login_fail'] = "Wrong userID / password";
 	        }*/
 	}
+
+
+if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+    
+     $fileName = $_FILES['file']['name'];
+     $fileTmpName = $_FILES['file']['tmp_name'];
+     $fileSize = $_FILES['file']['size'];
+     $fileError = $_FILES['file']['error'];
+     $fileType = $_FILES['file']['type'];
+    
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    
+    $allowed = array ('jpg', 'jpeg', 'png', 'pdf');
+    
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 1000000) {
+                $fileNameNew = uniqid('',true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: daycareprofile.php?uploadsuccess");
+            } else {
+                echo "Your file it too large!";
+            }
+        }
+            else {
+                echo "There was an error uploading your file!";
+            }
+        }
+    else {
+        echo "You cannot upload files of this type!";
+    }
+}
 
     /*else {
             $_SESSION['message'] = "Database error. Login failed!";
